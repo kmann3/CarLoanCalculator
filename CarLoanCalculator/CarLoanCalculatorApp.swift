61f72d878a0f3768a59244b0ed13cf49c059cc67
Kenny Mann <kmann@etherpunk.com>
2026-06-30 10:50:05 -0500

    Initial Incarnation
    Init incar 2. Unchecked these boxes on accident. Whoops.

---
+++ CarLoanCalculator/CarLoanCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarLoanCalculatorApp.swift
+//  CarLoanCalculator
+//
+//  Created by Kenny Mann on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct CarLoanCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
